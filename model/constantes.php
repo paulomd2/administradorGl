cc2d2aaaf364dbe998ee06fe528aa46d80b1eb7d
agenciamd2 <paulo.sergio@agencioamd2.com.br>
2015-01-12 18:52:44 -0200

    comum

---
+++ model/constantes.php
@@ -0,0 +1,15 @@
+<?php
+define('CON_ADMIN_HOST','localhost');
+define("CON_ADMIN_USER","root");
+define("CON_ADMIN_PASS","");
+define("CON_ADMIN_BASE","admingl");
+
+//Constantes de banco
+define("DB_ADMIN"," ".CON_ADMIN_BASE.".");
+
+//Constantes prefixo sistema
+define("TBL_ADMIN","");
+
+//Constantes de Tabela
+//Tabelas ADMIN
+define("TBL_USUARIOS",DB_ADMIN.TBL_ADMIN."usuarios ");
