27cad173a6e41926912d0060d989616271ddd7ed
lucasfreitasmd2 <lucas.freitas@agenciamd2.com.br>
2015-01-16 17:50:00 -0200

    Pasta include

---
+++ include/header.php
@@ -0,0 +1,29 @@
+<div class="center-user">
+    <div class="detail-user">
+        <figure class="avatar fl">
+            <a href="http://localhost/githubpaulo/administradorGl/painel.php"><img src="http://www.portalgl.com.br/imagens/logo_glevents.png" alt="Nome avatar" /></a>
+        </figure>
+        <div class="fr"></div>
+        <span><em>Seja Bem vindo</em></span>
+        <span>Nome Sobrenome</span>
+    </div>
+    <div class="barra-user">
+        <div class="busca">
+            <a href="#" onclick="javascript:mostraForm('formbusca');"><i class="icon icon-search"></i></a>
+            <form action="" method="get" class="form-busca-oculta" id="formbusca">
+                <input type="text" name="nome"/>
+                <select>
+                    <option>NOTÍCIA</option>
+                    <option>RELEASES</option>
+                    <option>EVENTOS</option>
+                </select>
+                <input type="submit" value="BUSCAR"/>
+            </form>
+        </div>
+        <div class="logout">
+            <a href="#" class="visu-site">Visualizar site</a>
+            <a href="#" class="sair">Sair</a>
+        </div>
+    </div>
+    <div class="cb"></div>
+</div>
---
+++ include/lateral.php
@@ -0,0 +1,24 @@
+<aside class="barra-lateral">
+    <ul>
+        <li class="hasub"><a href="#1"><i class="icon icon-file-text2"></i> Conteúdo </a>
+            <ul id="1" class="exemplo">
+                <li><a href="#" onclick="javascript:void(0);">Item</a></li>
+            </ul>
+        </li>
+        <li class="hasub"><a href="#2"><i class="icon icon-cog"></i> Administração</a>
+            <ul id="2">
+                <li><a href="#" onclick="javascript:void(0);">Item</a></li>
+            </ul>
+        </li>
+        <li class="hasub"><a href="#3"><i class="icon icon-user"></i> Usuários</a>
+            <ul id="3">
+                <li><a href="#" onclick="javascript:void(0);">Item</a></li>
+            </ul>
+        </li>
+        <li><a href="http://localhost/githubpaulo/administradorGl/noticias"><i class="icon icon-newspaper"></i> Notícias</a></li>
+        <li><a href="#"><i class="icon icon-newspaper"></i> Releases</a></li>
+        <li><a href="#"><i class="icon icon-tv"></i> Destaques</a></li>
+        <li><a href="#"><i class="icon icon-image"></i> Banners</a></li>
+        <li><a href="#"><i class="icon icon-upload2"></i> Uploads</a></li>
+    </ul>
+</aside>
