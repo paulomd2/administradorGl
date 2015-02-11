7409e44789124fffddd7eb4762f5758e573cd50d
lucasfreitasmd2 <lucas.freitas@agenciamd2.com.br>
2015-02-11 16:59:13 -0200

    E-mail do admin

---
+++ email.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Alterar sua senha</title>
+    </head>
+    <body background="#f1f2f2" style="background: #f1f2f2; font-family: 'Arial', sans-serif; color: #404040;">
+        <table style="border:0;padding:20px;margin:0;width:560px;margin:0 auto;border:1px solid #e0e0e0;border-radius:3px;background:#fff;text-align:center;">
+            <tr>
+                <td>
+                    <h1 style="font-weight: lighter; text-transform: uppercase;">Sua nova senha é:</h1>
+                </td>
+                <td>
+                    <img src="http://www.portalgl.com.br/imagens/logo_glevents.png" alt=""/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <p>
+                        SENHA
+                    </p>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <p>
+                        <a href="#" style="text-decoration: none;font-size: 12px; padding: 5px 10px; text-transform: uppercase; color: #fff; background: #999; border-radius: 3px; display: inline-block;">Ir para o site</a>
+                    </p>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
