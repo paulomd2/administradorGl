357d7a3c606a207f81e9b881e6a9758ab0f0c230
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-02-19 11:57:37 -0200

    conexão com o novo banco

--- model/constantesBanco.php
+++ model/constantesBanco.php
@@ -8,10 +8,10 @@ define("CON_ADMIN_BASE","admingl");
 */
 
 //MD2
-define('CON_ADMIN_HOST','192.185.215.211');
-define("CON_ADMIN_USER","agenciam_fagga");
+define('CON_ADMIN_HOST','108.167.188.128');
+define("CON_ADMIN_USER","agenc508_fagga");
 define("CON_ADMIN_PASS","fagga123mudar");
-define("CON_ADMIN_BASE","agenciam_administradorGl");
+define("CON_ADMIN_BASE","agenc508_administradorGl");
 
 //Constantes de banco
 define("DB_ADMIN"," ".CON_ADMIN_BASE.".");
