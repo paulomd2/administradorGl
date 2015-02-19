25c10511e35e89b7de8c73e6bc0e01bb7848ffd8
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-02-19 13:53:15 -0200

    ordenação do banner

--- banners/listaBannersAjax.php
+++ banners/listaBannersAjax.php
@@ -1,22 +1,29 @@
-<?php
+<div id="bannersordem">
+    <ul>
+        <?php
+        require_once '../model/banco.php';
+        require_once 'model/dao.php';
 
-require_once '../model/banco.php';
-require_once 'model/dao.php';
+        if (isset($_GET['count'])) {
+            $count = $_GET['count'];
+        } else {
+            $count = 100;
+        }
 
-if(isset($_GET['count'])){
-    $count = $_GET['count'];
-}else{
-    $count = 100;
-}
-
-$banners = $objBannersDao->listaBanners($count);
-
-for ($i = 0; $i < count($banners); $i++) {
-    echo '<tr>
-            <td>' . utf8_encode($banners[$i]["nome"]). '</td>
-            <td><img src="../images/' . $banners[$i]["imagem"] . '" alt="' . utf8_encode($banners[$i]["nome"]). '" title="' . utf8_encode($banners[$i]["nome"]). '" width="130" /></td>
-            <td><a href="altBanner.php?id=' . $banners[$i]['idBanner'] . '">Alterar</a></td>
-            <td><a href="javascript:delBanner(' . $banners[$i]["idBanner"] . ')">Excluir</a></td>
-          </tr>';
-}
-?>
+        $banners = $objBannersDao->listaBanners($count);
+        
+        for ($i = 0;$i < count($banners);$i++) {
+            echo '
+                    <li id="recordsArray_'.$banners[$i]["idBanner"].'">
+                        <div class = "lista_banner">
+                            <img src = "../images/'.$banners[$i]["imagem"].'" alt = "'.$banners[$i]["nome"].'" title = "'.$banners[$i]["nome"].'" width = "300" style = "float: left; margin-right: 10px;"/>
+                            <span>'.$banners[$i]["nome"].'</span><br/>
+                            <a href="altBanner.php?id=' . $banners[$i]['idBanner'] . '">Alterar</a> | <a href="javascript:delBanner(' . $banners[$i]["idBanner"] . ')">Excluir</a>
+                            </a>
+                        </div>
+                    </li>';
+        
+        }
+    ?>
+    </ul>
+</div>
