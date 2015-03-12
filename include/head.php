1511a5c19218507a9d86d70b245f46e7aa9c904f
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-03-12 18:13:17 -0300

    alteração para o jquery local

--- include/head.php
+++ include/head.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" type="text/css" href="../css/style.css" />
-<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="../js/jquery-2.1.3.js"></script>
 <script type="text/javascript" src="../js/default.js"></script>
