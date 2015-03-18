4b5dc07d7a2c8d96c1e0a93276d149dd7c7764fc
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-03-18 18:40:41 -0300

    icones do módulo de uploads

--- uploads/listaArquivos.php
+++ uploads/listaArquivos.php
@@ -9,7 +9,7 @@ $raiz = explode('/', $_SERVER['REQUEST_URI']);
 $raiz = $raiz[1];
 
 $arquivos = lista($caminho);
-$arquivos = $arquivos['arquivos'];
+@$arquivos = $arquivos['arquivos'];
 
 $caminho = 'http://' . $_SERVER["SERVER_NAME"] . '/' . $raiz . '/arquivos/' . $pasta;
 
@@ -19,6 +19,14 @@ for ($i = 0; $i < count($arquivos); $i++) {
     $icone = '';
     if ($extensao == '.jpeg' || $extensao == '.jpg' || $extensao == '.bmp' || $extensao == '.gif' || $extensao == '.png') {
         $icone = '<a href="' . $url . '" target="_blank"><img src="../arquivos/' . $pasta . '/' . $arquivos[$i] . '" style="width:150px" /></a>';
+    }elseif($extensao == '.doc' || $extensao == '.docx'){
+        $icone = '<a href="' . $url . '" target="_blank"><img src="../imagens/iconWord.png" style="width:150px" /></a>';
+    }elseif($extensao == '.xls' || $extensao == '.xla'){
+        $icone = '<a href="' . $url . '" target="_blank"><img src="../imagens/iconExcell.png" style="width:150px" /></a>';
+    }elseif($extensao == '.ppt' || $extensao == '.pps'){
+        $icone = '<a href="' . $url . '" target="_blank"><img src="../imagens/iconPowerPoint.png" style="width:150px" /></a>';
+    }elseif($extensao == '.pdf'){
+        $icone = '<a href="' . $url . '" target="_blank"><img src="../imagens/iconPdf.png" style="width:150px" /></a>';
     }
 
     echo '
