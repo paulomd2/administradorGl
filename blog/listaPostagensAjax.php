ed43264879200913ffae221a68e306bf2dfc76a2
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-03-23 14:30:02 -0300

    exibição de status em blog

--- blog/listaPostagensAjax.php
+++ blog/listaPostagensAjax.php
@@ -1,16 +1,23 @@
 <?php
-@session_start();
-//$idUsuario = $_SESSION['id'];
-$idUsuario = 1;
 
 require_once '../model/banco.php';
 require_once 'model/dao.php';
 
+if ($_SESSION['nivel'] == 3) {
+    $idUsuario = $_SESSION['id'];
+}else{
+    $idUsuario = 0;
+}
 $usuarios = $objBlogDao->listaPostagens($idUsuario);
 
 for ($i = 0; $i < count($usuarios); $i++) {
+    if ($usuarios[$i]['status'] == 1) {
+        $classe = "class='habilitado'";
+    } else {
+        $classe = "class='habilitado'";
+    }
 
-    echo '<tr>
+    echo '<tr ' . $classe . '>
             <td>' . $usuarios[$i]["titulo"] . '</td>
             <td><a href="altPostagem.php?id=' . $usuarios[$i]['idPostagem'] . '">Alterar</a></td>
             <td><a href="javascript:delPostagem(' . $usuarios[$i]["idPostagem"] . ')">Excluir</a></td>
